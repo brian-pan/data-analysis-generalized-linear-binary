f98f309373ec8dd9f7886b4d5ace59e1c4cadcd2
Brian Pan <brianpanzifan@gmail.com>
2020-02-22 15:42:42 -0500

    cond coding

--- data-analysis-glm-binary.R
+++ data-analysis-glm-binary.R
@@ -17,3 +17,5 @@ smoke[1:5,c('Age','Sex','Grade','RuralUrban','Race', 'Tried_cigarette_smkg_even'
 # Check the data format of the variable that to be predicted:
 smokeFormats[smokeFormats$colName == 'Tried_cigarette_smkg_even', ]
 
+# Change value 1 to yes, 2 to no:
+smoke$ifsmoked = factor(smoke$Tried_cigarette_smkg_even, levels = 1:2, labels = c('yes','no'))
